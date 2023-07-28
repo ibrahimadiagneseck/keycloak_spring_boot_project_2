56083361d247e4e3be76592f27a4b071fbfd73d9
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-07-28 13:26:55 +0000

    commit

--- products-app-kc/src/main/java/ma/enset/productsapp/sec/KeycloakAdapterConfig.java
+++ products-app-kc/src/main/java/ma/enset/productsapp/sec/KeycloakAdapterConfig.java
@@ -1,6 +1,9 @@
 package ma.enset.productsapp.sec;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +13,10 @@ public class KeycloakAdapterConfig {
     @Bean
     public KeycloakSpringBootConfigResolver springBootConfigResolver() { // dire a keycloak adapter, utilise la conf de springboot et non keycloak.json dans les resources
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    public KeycloakRestTemplate keycloakRestTemplate(KeycloakClientRequestFactory keycloakClientRequestFactory) { // dire a keycloak adapter, utilise la conf de springboot et non keycloak.json dans les resources
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
     }
 }
--- products-app-kc/src/main/java/ma/enset/productsapp/web/ProductController.java
+++ products-app-kc/src/main/java/ma/enset/productsapp/web/ProductController.java
@@ -1,13 +1,26 @@
 package ma.enset.productsapp.web;
 
 import ma.enset.productsapp.repositories.ProductRepository;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ProductController {
+
+    @Autowired
+    private KeycloakRestTemplate keycloakRestTemplate;
 
     @Autowired
     private ProductRepository productRepository;
@@ -24,8 +37,21 @@ public class ProductController {
     }
 
     @GetMapping("/suppliers")
-    public String suppliers(){
+    public String suppliers() {
+        keycloakRestTemplate.getForObject("http://localhost:8083/suppliers")
         return "suppliers";
+    }
+
+
+    @GetMapping("/jwt")
+    @ResponseBody
+    public Map<String, String> map(HttpServletRequest request) {
+        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();
+        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+        KeycloakSecurityContext keycloakSecurityContext = principal.getKeycloakSecurityContext();
+        Map<String, String> map = new HashMap<>();
+        map.put("access_token", keycloakSecurityContext.getTokenString());
+        return map;
     }
 
 }
